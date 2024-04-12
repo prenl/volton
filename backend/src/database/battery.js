c70ef7e1741c5749e5ccbfd3ff107077eb58b8d1
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-04-12 23:33:09 +0500

    feat: add battery model

--- backend/src/database/battery.js
+++ backend/src/database/battery.js
@@ -1,3 +1,54 @@
 const mongoose = require("mongoose");
 
-// Here will be battery model
+const batterySchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    brand: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    category_id: {
+        type: Number,
+        required: true,
+    },
+    capacity: {
+        type: Number,
+        required: true,
+    },
+    voltage: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: false,
+        default: null,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Battery = mongoose.model("Battery", batterySchema);
+
+module.exports = Battery;
