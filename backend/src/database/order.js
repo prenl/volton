8b04c16d3312211fef7be4eaf8ac25d2d530cd83
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-04-12 23:35:39 +0500

    feat: add order model

--- backend/src/database/order.js
+++ backend/src/database/order.js
@@ -1,3 +1,33 @@
 const mongoose = require("mongoose");
 
-// Here will be order model
+const orderSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    user_id: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    items: {
+        type: Array,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
