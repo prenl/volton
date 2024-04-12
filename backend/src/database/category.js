ab2a22310560c081cab247e3e8adca02fe139dc3
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-04-12 23:34:16 +0500

    feat: add category model

--- backend/src/database/category.js
+++ backend/src/database/category.js
@@ -1,3 +1,30 @@
 const mongoose = require("mongoose");
 
-// Here will be category model
+const categorySchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+        default: null,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
