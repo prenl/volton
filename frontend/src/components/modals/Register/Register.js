29734581aef5f098ba97aaeff6e32fff29ea8e63
Damir <muxamedkali@gmail.com>
2024-04-29 19:17:40 +0500

    Index stylesheet added mixins; Changed logo;

--- frontend/src/components/modals/Register/Register.js
+++ frontend/src/components/modals/Register/Register.js
@@ -38,25 +38,25 @@ const Register = ({ isOpen, onClose }) => {
                 <div className="modal-overlay">
                     <div className="modal-content">
                         <button className="close-button" onClick={onClose}>X</button>
-                        <h2>Register</h2>
+                        <h2>Регистрация</h2>
                         <form onSubmit={handleSubmit}>
                             <div className="form-group">
-                                <label htmlFor="firstName">First Name:</label>
-                                <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} required />
+                                <label htmlFor="firstName">Ваше Имя:</label>
+                                <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} required placeholder='Ваше Имя' />
                             </div>
                             <div className="form-group">
-                                <label htmlFor="lastName">Last Name:</label>
-                                <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} required />
+                                <label htmlFor="lastName">Ваша Фамилия:</label>
+                                <input type="text" id="lastName" name="lastName" value={formData.lastName} onChange={handleChange} required placeholder='Ваша Фамилия' />
                             </div>
                             <div className="form-group">
-                                <label htmlFor="email">Email:</label>
-                                <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+                                <label htmlFor="email">Электронная почта:</label>
+                                <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required placeholder='Ваша электронная почта' />
                             </div>
                             <div className="form-group">
-                                <label htmlFor="password">Password:</label>
-                                <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
+                                <label htmlFor="password">Пароль:</label>
+                                <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required placeholder='Пароль' />
                             </div>
-                            <button type="submit">Register</button>
+                            <button type="submit">Зарегистрироваться</button>
                         </form>
                     </div>
                 </div>
